475942024f40844920c44efba0660c25ba0af12f
raghavchugh21 <65908705+raghavchugh21@users.noreply.github.com>
2021-03-08 09:03:45 +0530

    Added Single Article Page

--- urls.py
+++ urls.py
@@ -16,10 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, re_path, include
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'^articles', include('articles.urls')),
-    re_path(r'^about', views.about),
-    re_path(r'$',views.homepage),
+    path('articles/', include('articles.urls')),
+    path('about/', views.about),
+    path('',views.homepage)
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
